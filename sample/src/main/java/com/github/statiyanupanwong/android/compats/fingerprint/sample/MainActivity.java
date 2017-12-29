ca32d421760442e86848a757892022cd2fde55ae
Supasin Tatiyanupanwong <s.tatiyanupanwong@gmail.com>
2017-12-29 07:26:41 +0700

    Updating sample.

--- sample/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/sample/MainActivity.java
+++ sample/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/sample/MainActivity.java
@@ -1,13 +1,41 @@
 package com.github.statiyanupanwong.android.compats.fingerprint.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.statiyanupanwong.android.compats.fingerprint.FingerprintCompat;
+import com.github.statiyanupanwong.android.compats.fingerprint.FingerprintResponse;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        FingerprintCompat.AuthenticationCallback {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.helloWorld);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mButton.equals(v)) {
+            FingerprintCompat.with(this).authenticate(this);
+        }
+    }
+
+    @Override
+    public void onAuthenticationResponse(FingerprintResponse response) {
+
+    }
+
+    @Override
+    public void onAuthenticationFailure(Throwable throwable) {
+
     }
 }
