896a81896ada3f671a1557b8014962543fa80f88
Supasin Tatiyanupanwong <s.tatiyanupanwong@gmail.com>
2017-12-29 15:33:19 +0700

    Adding license notice

--- FingerprintCompat/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/FingerprintCompat.java
+++ FingerprintCompat/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/FingerprintCompat.java
@@ -1,13 +1,30 @@
+/*
+ * Copyright (C) 2017 Supasin Tatiyanupanwong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.statiyanupanwong.android.compats.fingerprint;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 public abstract class FingerprintCompat implements FingerprintCompatInterface {
-    public static FingerprintCompat of(Context context) {
+    public static FingerprintCompat of(@NonNull Context context) {
         return new FingerprintCompatImpl(context);
     }
 
-    public static boolean isAvailable(Context context) {
+    public static boolean isAvailable(@NonNull Context context) {
         return of(context).isAvailable();
     }
 
@@ -26,17 +43,17 @@ public abstract class FingerprintCompat implements FingerprintCompatInterface {
     abstract void decryptImpl(String toDecrypt, DecryptionCallback callback);
 
     @Override
-    public void authenticate(AuthenticationCallback callback) {
+    public void authenticate(@NonNull AuthenticationCallback callback) {
         authenticateImpl(callback);
     }
 
     @Override
-    public void encrypt(String toEncrypt, EncryptionCallback callback) {
+    public void encrypt(@NonNull String toEncrypt, @NonNull EncryptionCallback callback) {
         encryptImpl(toEncrypt, callback);
     }
 
     @Override
-    public void decrypt(String toDecrypt, DecryptionCallback callback) {
+    public void decrypt(@NonNull String toDecrypt, @NonNull DecryptionCallback callback) {
         decryptImpl(toDecrypt, callback);
     }
 
--- sample/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/sample/MainActivity.java
+++ sample/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/sample/MainActivity.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright (C) 2017 Supasin Tatiyanupanwong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.statiyanupanwong.android.compats.fingerprint.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.statiyanupanwong.android.compats.fingerprint.FingerprintCompat;
 import com.github.statiyanupanwong.android.compats.fingerprint.FingerprintResponse;
@@ -12,15 +29,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FingerprintCompat.EncryptionCallback, FingerprintCompat.DecryptionCallback {
 
     private Button mButton;
-    private String mInitialText = "test";
     private String mEncrypted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mInitialText = "test";
 
         mButton = findViewById(R.id.helloWorld);
         mButton.setOnClickListener(this);
@@ -30,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (mButton.equals(v)) {
             if (mEncrypted == null) {
-                FingerprintCompat.of(this).encrypt(mInitialText, this);
+                FingerprintCompat.of(this).encrypt("Test", this);
             } else {
                 FingerprintCompat.of(this).decrypt(mEncrypted, this);
             }
@@ -52,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onDecryptionResponse(FingerprintResponse response) {
         if (response.isSuccessful()) {
-            String decrypted = response.getData();
+            Toast.makeText(getApplicationContext(), response.getData(), Toast.LENGTH_SHORT).show();
         }
     }
 
