a1acd41b090375611d162302f4f94b9c52497605
Supasin Tatiyanupanwong <s.tatiyanupanwong@gmail.com>
2018-01-02 19:45:38 +0700

    Update sample code

--- sample/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/sample/MainActivity.java
+++ sample/src/main/java/com/github/statiyanupanwong/android/compats/fingerprint/sample/MainActivity.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.github.statiyanupanwong.android.compats.fingerprint.FingerprintCompat;
 import com.github.statiyanupanwong.android.compats.fingerprint.FingerprintResponse;
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FingerprintCompat.EncryptionCallback, FingerprintCompat.DecryptionCallback {
 
     private Button mButton;
+    private TextView mTextView;
+
+    private String mInitialText;
     private String mEncrypted;
 
     @Override
@@ -36,15 +39,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton = findViewById(R.id.helloWorld);
+        mInitialText = "Test";
+
+        mButton = findViewById(R.id.touch_me);
+        mTextView = findViewById(R.id.status);
         mButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (mButton.equals(v)) {
+            mButton.setEnabled(false);
+            mTextView.setText(R.string.touch_sensor);
             if (mEncrypted == null) {
-                FingerprintCompat.of(this).encrypt("Test", this);
+                FingerprintCompat.of(this).encrypt(mInitialText, this);
             } else {
                 FingerprintCompat.of(this).decrypt(mEncrypted, this);
             }
@@ -55,23 +63,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onEncryptionResponse(FingerprintResponse response) {
         if (response.isSuccessful()) {
             mEncrypted = response.getData();
+            String str = "Initial text of \'" +
+                    mInitialText +
+                    "\' was encrypted to \'" +
+                    mEncrypted +
+                    "\'";
+            mTextView.setText(str);
+            mButton.setEnabled(true);
         }
     }
 
     @Override
     public void onEncryptionFailure(Throwable throwable) {
+        mTextView.setText(R.string.encryption_failed);
+        mButton.setEnabled(true);
         throwable.printStackTrace();
     }
 
     @Override
     public void onDecryptionResponse(FingerprintResponse response) {
         if (response.isSuccessful()) {
-            Toast.makeText(getApplicationContext(), response.getData(), Toast.LENGTH_SHORT).show();
+            String str = "Encrypted text of \'" +
+                    mEncrypted +
+                    "\' was decrypted to \'" +
+                    response.getData() +
+                    "\'";
+            mTextView.setText(str);
+            mButton.setEnabled(true);
         }
     }
 
     @Override
     public void onDecryptionFailure(Throwable throwable) {
+        mTextView.setText(R.string.decryption_failed);
+        mButton.setEnabled(true);
         throwable.printStackTrace();
     }
 }
